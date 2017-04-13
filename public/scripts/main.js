0fb4031091e48371da09da1050167d39fdc060ef
herno <russo.hernan@gmail.com>
2017-04-13 19:11:51 -0300

    main script refactor * Add more comments to code * Refactor setup handler function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,30 +1,46 @@
 (function() {
   'use strict';
+
+  //  Set up constants
+  const SCRIPT_CHAPTERS_BASE_ROUTE = 'scripts/chapters/'
+  const SCRIPT_FILE_TYPE = '.js'
+
+  /**
+   * Setup an event handler for each chapter (li) item
+   * @return {undefined}
+   */
+  function setupHandlers() {
+    /** @type {object} nodeList Array-like list of dom elements] */
+    let nodeList = document.querySelectorAll('#chapters ul li');
+
+    for (let i = 0; i < nodeList.length; i++) {
+      let node = nodeList.item(i)
+      node.addEventListener('click', addScript);
+
+      // log using ecma6 template literals
+      console.log(`handler ${node.dataset.id} setup`);
+    }
+  }
+
+  /**
+  * Create and insert script in head tag
+  * @param {string} event html click event
+  * @return
+  */
+  function addScript(event) {
+    let scriptTag = document.createElement('script');
+    scriptTag.src = SCRIPT_CHAPTERS_BASE_ROUTE + event.target.dataset.script + SCRIPT_FILE_TYPE;
+    document.body.appendChild(scriptTag);
+  }
+
+  /********************************************
+   * On Load attach listener to list elements *
+   * Also, log the loaded event               *
+   ********************************************/
   document.addEventListener('DOMContentLoaded', function() {
     // For tracking purposes
     console.log('document loaded');
-
-    //  Set up constants
-    const SCRIPT_BASE_ROUTE = 'scripts/'
-    const SCRIPT_FILE_TYPE = '.js'
-
-    function setupHandlers() {
-      let nodeList = document.querySelector('.chapters.li').childNodes;
-      nodeList.map(function(node){
-        node.addEventListener('click', addScript(e));
-      })
-    }
-
-    /**
-    * Create and insert script in head tag
-    * @param {string} event html click event
-    * @return
-    */
-    function addScript(event) {
-      let scriptTag = document.createElement('script');
-      scriptTag.src = SCRIPT_BASE_ROUTE + event.target.dataset.script + SCRIPT_FILE_TYPE;
-      document.body.appendChild(scriptTag);
-    }
-
+    setupHandlers();
   });
+
 })();
